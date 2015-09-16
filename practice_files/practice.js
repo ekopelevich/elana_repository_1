2752b4b3b509180c010324f8ec98e28d50a51dec
Elana Kopelevich <ekopelevich@gmail.com>
2015-09-16 10:21:08 -0600

    Added some practice exercises
    Reverse a word and reverse the words in a sentence

---
+++ practice_files/practice.js
@@ -0,0 +1,35 @@
+//Reverse a word
+
+// var specialWord = "elanabanana";
+//
+// function stringReverse (word) {
+//     var i, reverseString;
+//     var reverseArr = [];
+//     for (i = 0; i < word.length; i++){
+//         reverseArr.unshift(word.charAt(i));
+//         reverseString = reverseArr.toString();
+//         finalRevString = reverseString.replace(/,/g,"");
+//     }
+//     console.log(finalRevString);
+// }
+//
+// stringReverse(specialWord);
+
+
+//Reverse the words in a sentence
+
+// var specialSentence = "Elana is Smart";
+//
+// function sentenceSwitcher (original, separator) {
+//   var stringArray = original.split(separator)
+//                             .reverse()
+//                             .toString()
+//                             .replace(/,/g, " ");
+//
+//   console.log(original);
+//   console.log(stringArray);
+// }
+//
+// sentenceSwitcher(specialSentence, " ");
+
+//Calculate a remainder (given a numerator and denominator)
